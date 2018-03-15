3009188b082e8d652bce59093f21bed2513a625b
TaybaMuneer <35809328+TaybaMuneer@users.noreply.github.com>
2018-03-15 16:26:38 +0000

    breakout js 2

--- breakout.js
+++ breakout.js
@@ -14,6 +14,23 @@ var paddleX = (canvas.width-paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
 
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+var bricks = [];
+for(c=0; c<brickColumnCount; c++) {
+	bricks[c] = [];
+	for(r=0; r<brickRowCount; r++) {
+		bricks[c][r] = { x: 0, y: 0, status: 1 };
+	}
+}
+
+
+	
 
 function drawBall() {
 	ctx.beginPath();
@@ -31,11 +48,33 @@ function drawPaddle() {
 	ctx.closePath();
 }
 
+function drawBricks() {
+	for(c=0; c<brickColumnCount; c++) {
+		for(r=0; r<brickRowCount; r++) {
+			if(bricks[c][r].status == 1) {
+			var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+			var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+			bricks[c][r].x = brickX;
+			bricks[c][r].y = brickY;
+			ctx.beginPath();
+			ctx.rect(brickX, brickY, brickWidth, brickHeight);
+			ctx.fillStyle = "0095DD";
+			ctx.fill();
+			ctx.closePath();
+		}
+	}
+	
+}
+}
+
+
 
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
 	drawPaddle();
+	drawBricks();
+	collisionDetection();
 	
 	x += dx;
 	y += dy;
@@ -44,8 +83,18 @@ function draw() {
 		dx = -dx;
 	}
 
-	if(y + dy > canvas.height-ballRadius || y + dy < ballRadius){
+	if(y + dy < ballRadius) {
 		dy = -dy;
+	} else if(y + dy > canvas.height-ballRadius) {
+		if(x > paddleX && x < paddleX + paddleWidth) {
+			dy = -dy;
+		}
+		else {
+			alert("GAME OVER");
+			x = canvas.width/2;
+			y = canvas.height-30;
+			document.location.reload();
+		}
 	}
 
 	if(rightPressed && paddleX < canvas.width-paddleWidth){
@@ -75,6 +124,19 @@ function keyUpHandler(e) {
 	}
 	else if(e.keyCode == 37) {
 		leftPressed = false;
+	}
+}
+function collisionDetection() {
+	for(c=0; c<brickColumnCount; c++) {
+		for(r=0; r<brickRowCount; r++) {
+			var b = bricks[c][r];
+			if(b.status == 1) {
+				if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+				dy = -dy;
+				b.status = 0;
+				}
+			}
+		}
 	}
 }
 	
